84843834b6f61c712ceb616fe2d685b3d4236cce
Resn4R <purview-joust.0x@icloud.com>
2023-11-15 18:18:50 +0000

    Initial Commit

---
+++ Stats Calculator TDD/Stats_Calculator_TDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stats_Calculator_TDDApp.swift
+//  Stats Calculator TDD
+//
+//  Created by Vito Borghi on 15/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Stats_Calculator_TDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
