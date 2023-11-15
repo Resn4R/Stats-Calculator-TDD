8000736c4416fdcdd338f0729f0a56ce75866fbc
Resn4R <purview-joust.0x@icloud.com>
2023-11-15 18:32:03 +0000

    one test passed

---
+++ Stats Calculator TDDTests/StatCalculator.swift
@@ -0,0 +1,15 @@
+//
+//  StatCalculator.swift
+//  Stats Calculator TDD
+//
+//  Created by Vito Borghi on 15/11/2023.
+//
+
+import Foundation
+
+struct StatCalculator {
+    
+    func getMinimumValue(in inputArray: [Int]) -> Int {
+        inputArray.min()!
+    }
+}
--- Stats Calculator TDDTests/Stats_Calculator_TDDTests.swift
+++ Stats Calculator TDDTests/Stats_Calculator_TDDTests.swift
@@ -9,28 +9,19 @@ import XCTest
 @testable import Stats_Calculator_TDD
 
 final class Stats_Calculator_TDDTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    /*
+     process a sequence of ints to determine the following:
+     - minimum value
+     - maximum value
+     - number of elements in the sequence
+     - average value
+     */
+    
+    func testGivenAnArrayOfIntsReturnsMinimumValue() {
+        let calculator = StatCalculator()
+        
+        let sampleArray = [3,7,5,2,-2,6,-6]
+        
+        XCTAssertEqual(calculator.getMinimumValue(in: sampleArray), -6)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
